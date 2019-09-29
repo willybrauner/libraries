809e26db899a863c66dc0b0123ff595614db4f4d
Willy <mail@willybrauner.com>
2019-09-29 15:07:27 +0200

    first commit

---
+++ packages/front-react/components/grid-layout-component/example/Main.tsx
@@ -0,0 +1,29 @@
+import './Main.less';
+import {GridLayout} from '../src/GridLayout';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+declare var module: any;
+// active HMR
+if (module.hot) module.hot.accept();
+
+export const App = () => {
+  return (
+    <div className="App">
+      <div className="App_wrapper">
+        <h1 className="App_title">React Grid Layout Component</h1>
+        <p>Just press "G Key" on your keyboard to toggle the grid. 💪</p>
+      </div>
+
+      {/*Instance of Grid Layout here*/}
+      <GridLayout
+        showGridByDefault={true}
+        gutterSize={20}
+        //maxWidth={1024}
+      />
+    </div>
+  );
+};
+
+// Render our app into AppContainer tag
+ReactDOM.render(<App />, document.getElementById('AppContainer'));
