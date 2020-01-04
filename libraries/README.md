125c6d7e69f130a4f867ac9d0c5a6166d04fb2c9
Willy <mail@willybrauner.com>
2020-01-04 18:26:48 +0100

    add standelon libraries

---
+++ libraries/README.md
@@ -0,0 +1,17 @@
+# @wb/undefined
+
+## About
+
+## Installation
+
+```shell script
+$ npm install @wb/undefined
+```
+
+## Credits
+
+© Willy Brauner
+
+## Licence
+
+MIT
---
+++ libraries/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * libraries
+ * @description:
+ */
+export { GridLayout } from "@wb/react-grid-layout-component";
+export { EListener, useBoundingClientRect } from "@wb/use-bounding-client-rect";
+
+// TODO add interface
+export { useWindowSize } from "@wb/use-window-size";
+export { merge } from "@wb/utils-array";
