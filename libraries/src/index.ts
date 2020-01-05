6d7917314b5beb60681ce8c473ae88e0a8f0cbcf
Willy <mail@willybrauner.com>
2020-01-05 15:55:19 +0100

    export interface

--- libraries/src/index.ts
+++ libraries/src/index.ts
@@ -2,7 +2,8 @@
  * libraries standalone
  * @description: export every libs
  */
+
 // prettier-ignore
 export { default as GridLayout} from "@wbe/react-grid-layout";
-export { default as useBoundingClientRect } from "@wbe/use-bounding-client-rect";
-export { default as useWindowSize } from "@wbe/use-window-size";
+export { default as useBoundingClientRect, EListener } from "@wbe/use-bounding-client-rect";
+export { default as useWindowSize, IWindowSize } from "@wbe/use-window-size";
