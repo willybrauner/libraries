9962111f58fc7495276be9e67228958b292bdf62
Willy <mail@willybrauner.com>
2020-02-21 15:54:00 +0100

    fix storybook presentation

--- packages/storybook/.storybook/addons.js
+++ packages/storybook/.storybook/addons.js
@@ -1,6 +1,9 @@
+import "storybook-readme/register";
 import registerWithPanelTitle from "storybook-readme/registerWithPanelTitle";
+registerWithPanelTitle("readme");
+import "storybook-readme";
+import "@storybook/addon-knobs/register";
 import "@storybook/addon-storysource/register";
-registerWithPanelTitle("Docs");
 // import '@storybook/addon-actions/register';
 // import '@storybook/addon-links/register';
 // import '@storybook/addon-notes/register';
