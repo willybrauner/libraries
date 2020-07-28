cc4844deaced0e4b3d029c3740ec9f60db57681a
willybrauner <mail@willybrauner.com>
2020-07-28 09:59:38 +0200

    Externalize Vimeo player - TODO - create a new module for each video type ?

--- storybook/stories/react-components/react-video-player.stories.tsx
+++ storybook/stories/react-components/react-video-player.stories.tsx
@@ -1,9 +1,10 @@
 import "../../global-style.css";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { storiesOf } from "@storybook/react";
 import README from "@wbe/react-video-player/README.md";
 // @ts-ignore
 import VideoPlayer, {
+  EVideoPlayState,
   EVideoType
 } from "../../../packages/react-components/react-video-player/src/index";
 import FakeDataUtils, {
@@ -14,43 +15,41 @@ import FakeDataUtils, {
 const storyName = "react-video-player";
 const debug = require("debug")(`lib:${storyName}`);
 
-/**
- * Demo
- */
-const Youtube = () => {
-  return (
-    <div>
-      <VideoPlayer
-        type={EVideoType.YOUTUBE}
-        url={`https://youtu.be/${FakeDataUtils.getVideoId(
-          DataEVideoType.YOUTUBE
-        )}`}
-      />
-    </div>
-  );
-};
-
-const Vimeo = () => {
-  return (
-    <div>
-      <VideoPlayer
-        type={EVideoType.VIMEO}
-        url={`https://vimeo.com/${FakeDataUtils.getVideoId(
-          DataEVideoType.VIMEO
-        )}`}
-      />
-    </div>
-  );
-};
-
-/**
- * Config
- */
 storiesOf(`react-components/${storyName}`, module)
   .addParameters({
     readme: {
       sidebar: README
     }
   })
-  //.add("youtube", () => <Youtube />)
-  .add("vimeo", () => <Vimeo />);
+  // .add("youtube", () => (
+  //   <VideoPlayer
+  //     type={EVideoType.YOUTUBE}
+  //     url={`https://youtu.be/${FakeDataUtils.getVideoId(
+  //       DataEVideoType.YOUTUBE
+  //     )}`}
+  //   />
+  // ))
+  .add("vimeo", () => {
+    const [play, setPlay] = useState(false);
+    const url = useMemo(
+      () =>
+        `https://vimeo.com/${FakeDataUtils.getVideoId(DataEVideoType.VIMEO)}`,
+      []
+    );
+
+    return (
+      <>
+        <button onClick={() => setPlay(!play)}>
+          {play ? "pause" : "play"}
+        </button>
+
+        <VideoPlayer
+          className={"Stories_videoPlayer"}
+          type={EVideoType.VIMEO}
+          url={url}
+          playState={play ? EVideoPlayState.PLAY : EVideoPlayState.PAUSE}
+          showControls={false}
+        />
+      </>
+    );
+  });
