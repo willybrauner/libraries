d3ac4b11d0e562dd3d790bb04e4c5f62a584d2d2
willybrauner <mail@willybrauner.com>
2020-08-30 11:49:37 +0200

    Link react-video-player to storybook & libraries standelone

--- storybook/stories/react-components/react-video-player.stories.tsx
+++ storybook/stories/react-components/react-video-player.stories.tsx
@@ -1,113 +1,147 @@
 import "../../global-style.css";
 import React, { useMemo, useState } from "react";
 import { storiesOf } from "@storybook/react";
-//import README from "@wbe/react-video-player/README.md";
-// @ts-ignore
+import README from "@wbe/react-video-player/README.md";
 import FakeDataUtils, {
-  EVideoType,
   EVideoType as DataEVideoType
 } from "@wbe/fake-data-utils";
-// @ts-ignore
-import { VimeoVideo } from "../../../packages/react-components/react-video-player/src/VimeoVideo";
-// @ts-ignore
-import { NativeVideo } from "../../../packages/react-components/react-video-player/src/NativeVideo";
-// @ts-ignore
-import { YoutubeVideo } from "../../../packages/react-components/react-video-player/src/YoutubeVideo";
+import VideoPlayer, { EVideoType } from "@wbe/react-video-player";
 
-// set story name
+// set story name$
 const storyName = "react-video-player";
 const debug = require("debug")(`lib:${storyName}`);
+
+/**
+ *
+ */
+
+/**
+ * Youtube
+ * @constructor
+ */
+const YoutubeVideoTest = () => {
+  const [play, setPlay] = useState<boolean>(false);
+  const [id, setId] = useState<string>(
+    FakeDataUtils.getVideoId(DataEVideoType.YOUTUBE)
+  );
+  const switchVideo = () => {
+    setId(FakeDataUtils.getVideoId(DataEVideoType.YOUTUBE));
+  };
+
+  return (
+    <>
+      <button onClick={switchVideo}>switch video</button>
+      <button onClick={() => setPlay(!play)}>{play ? "pause" : "play"}</button>
+      <VideoPlayer
+        type={EVideoType.YOUTUBE}
+        className={`${storyName}_youtube`}
+        id={id}
+        play={play}
+        style={{ width: 400, height: 300 }}
+        controls={false}
+        muted={false}
+        onPause={e => debug("pause callback", e)}
+        onPlay={e => debug("play callback", e)}
+        onEnded={e => debug("ended callback", e)}
+        onReady={e => debug("ready callback", e)}
+      />
+    </>
+  );
+};
+
+/**
+ * Vimeo
+ */
+const VimeoVideoTest = () => {
+  const [play, setPlay] = useState<boolean>(false);
+  const [id, setId] = useState<string>(
+    FakeDataUtils.getVideoId(DataEVideoType.VIMEO)
+  );
+  const switchVideo = () => {
+    setId(FakeDataUtils.getVideoId(DataEVideoType.VIMEO));
+  };
+
+  return (
+    <>
+      <button onClick={switchVideo}>switch video</button>
+      <button onClick={() => setPlay(!play)}>{play ? "pause" : "play"}</button>
+      <VideoPlayer
+        type={EVideoType.VIMEO}
+        className={`${storyName}_vimeo`}
+        id={id}
+        play={play}
+        style={{ width: 400, height: 300 }}
+        controls={true}
+        muted={true}
+        autoPlay={true}
+        onPause={e => debug("pause callback", e)}
+        onPlay={e => debug("play callback", e)}
+        onEnded={e => debug("ended callback", e)}
+        onReady={e => debug("ready callback", e)}
+      />
+    </>
+  );
+};
+
+/**
+ * Native
+ */
+const NativeVideoTest = () => {
+  const [play, setPlay] = useState<boolean>(false);
+
+  // TODO updater la liste de fake data utils
+  const url =
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
+
+  return (
+    <>
+      <button onClick={() => setPlay(!play)}>{play ? "pause" : "play"}</button>
+      <VideoPlayer
+        type={EVideoType.NATIVE}
+        className={`${storyName}_native`}
+        url={url}
+        play={true}
+        style={{ width: 400, display: "block" }}
+        controls={false}
+        muted={true}
+        autoPlay={true}
+        onPause={e => debug("pause callback", e)}
+        onPlay={e => debug("play callback", e)}
+        onEnded={e => debug("ended callback", e)}
+        onReady={e => debug("ready callback", e)}
+      />
+    </>
+  );
+};
 
 storiesOf(`react-components/${storyName}`, module)
   .addParameters({
     readme: {
-      //   sidebar: README
+      sidebar: README
     }
   })
   .add("native", () => {
-    const [play, setPlay] = useState<boolean>(false);
-
-    // TODO updater la liste de fake data utils
-    const url =
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
-
     return (
       <>
-        <button onClick={() => setPlay(!play)}>
-          {play ? "pause" : "play"}
-        </button>
-        <NativeVideo
-          className={`${storyName}_native`}
-          url={url}
-          play={play}
-          autoPlay={true}
-          muted={true}
-          controls={true}
-          style={{ width: 300 }}
-          onPause={() => debug("pause callback")}
-          onPlay={() => debug("play callback")}
-          onEnded={() => debug("ended callback")}
-          onCanPlay={() => debug("canplay callback")}
-          poster={FakeDataUtils.getResponsiveImageData()[0].url}
-        />
+        <NativeVideoTest />
       </>
     );
   })
 
   .add("vimeo", () => {
-    const [play, setPlay] = useState<boolean>(true);
-    const url = useMemo(() => {
-      return `https://vimeo.com/${FakeDataUtils.getVideoId(
-        DataEVideoType.VIMEO
-      )}`;
-    }, []);
     return (
       <>
-        <button onClick={() => setPlay(!play)}>
-          {play ? "pause" : "play"}
-        </button>
-        <VimeoVideo
-          className={`${storyName}_vimeo`}
-          url={url}
-          play={play}
-          style={{ width: 300 }}
-          controls={true}
-          autoPlay={true}
-          muted={true}
-          loop={false}
-          onPause={() => debug("pause callback")}
-          onPlay={() => debug("play callback")}
-          onEnded={() => debug("ended callback")}
-          onLoaded={() => debug("loaded callback")}
-        />
+        <VimeoVideoTest />
+        <VimeoVideoTest />
       </>
     );
   })
 
   .add("youtube", () => {
-    const [play, setPlay] = useState<boolean>(false);
-
-    const url = useMemo(() => {
-      return `https://youtu.be/${FakeDataUtils.getVideoId(
-        DataEVideoType.YOUTUBE
-      )}`;
-    }, [play]);
     return (
       <>
-        <button onClick={() => setPlay(!play)}>
-          {play ? "pause" : "play"}
-        </button>
-        <YoutubeVideo
-          className={`${storyName}_youtube`}
-          id={FakeDataUtils.getVideoId(DataEVideoType.YOUTUBE)}
-          url={url}
-          play={play}
-          style={{ width: 300 }}
-          controls={false}
-          // onPause={() => debug("pause callback")}
-          // onPlay={() => debug("play callback")}
-          // onEnded={() => debug("ended callback")}
-        />
+        <YoutubeVideoTest />
+        <YoutubeVideoTest />
       </>
     );
   });
