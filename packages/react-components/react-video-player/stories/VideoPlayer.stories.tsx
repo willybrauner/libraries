5e8e28f95d1dcd19d02a78e42be3490ed26de033
willybrauner <mail@willybrauner.com>
2020-09-14 00:54:35 +0200

    Fixed react-video-player story

--- packages/react-components/react-video-player/stories/VideoPlayer.stories.tsx
+++ packages/react-components/react-video-player/stories/VideoPlayer.stories.tsx
@@ -1,7 +1,7 @@
 // @ts-ignore
 // prettier-ignore
-import VideoPlayer, { EVideoType as EVideoPlayerType } from "../src/VideoPlayer";
-import FakeDataUtils, { EVideoType } from "@wbe/fake-data-utils";
+import VideoPlayer, { EVideoType } from "../src/VideoPlayer";
+import FakeDataUtils, { EFakeVideoType } from "@wbe/fake-data-utils";
 import React, { CSSProperties } from "react";
 import "../../../../storybook/global-style.css";
 
@@ -10,7 +10,7 @@ const debug = require("debug")(`lib:${storyName}`);
 
 // VideoPlayer props interface
 interface IProps {
-  type: EVideoPlayerType;
+  type: EVideoType;
   id?: string;
   url?: string;
   play: boolean;
@@ -34,8 +34,8 @@ export default {
   title: `react-components/${storyName}`,
   component: Template,
   args: {
-    type: EVideoPlayerType.YOUTUBE,
-    id: FakeDataUtils.getVideoId(EVideoType.YOUTUBE),
+    type: EVideoType.YOUTUBE,
+    id: FakeDataUtils.getVideoId(EFakeVideoType.YOUTUBE),
     url: null,
     play: true,
     style: { width: 400, height: 300 },
@@ -45,18 +45,18 @@ export default {
     muted: false,
     playsInline: true,
     className: "Youtube",
-    onReady: e => debug("onReady callback", e),
-    onPlay: e => debug("onPlay callback", e),
-    onPause: e => debug("onPause callback", e),
-    onEnded: e => debug("onEnded callback", e)
-  } as IProps
+    onReady: (e) => debug("onReady callback", e),
+    onPlay: (e) => debug("onPlay callback", e),
+    onPause: (e) => debug("onPause callback", e),
+    onEnded: (e) => debug("onEnded callback", e),
+  } as IProps,
 };
 
 export const Vimeo = Template.bind({});
 Vimeo.storyName = "vimeo";
 Vimeo.args = {
-  type: EVideoPlayerType.VIMEO,
-  id: FakeDataUtils.getVideoId(EVideoType.VIMEO),
+  type: EVideoType.VIMEO,
+  id: FakeDataUtils.getVideoId(EFakeVideoType.VIMEO),
   url: null,
   play: true,
   style: { width: 400, height: 300 },
@@ -66,17 +66,17 @@ Vimeo.args = {
   muted: false,
   playsInline: true,
   className: "Vimeo",
-  onReady: e => debug("onReady callback", e),
-  onPlay: e => debug("onPlay callback", e),
-  onPause: e => debug("onPause callback", e),
-  onEnded: e => debug("onEnded callback", e)
+  onReady: (e) => debug("onReady callback", e),
+  onPlay: (e) => debug("onPlay callback", e),
+  onPause: (e) => debug("onPause callback", e),
+  onEnded: (e) => debug("onEnded callback", e),
 };
 
 export const Native = Template.bind({});
 Native.storyName = "native";
 Native.args = {
-  type: EVideoPlayerType.NATIVE,
-  url: FakeDataUtils.getVideoUrl(EVideoType.NATIVE),
+  type: EVideoType.NATIVE,
+  url: FakeDataUtils.getVideoUrl(EFakeVideoType.NATIVE),
   play: true,
   style: { width: 400, height: 300 },
   controls: false,
@@ -85,8 +85,8 @@ Native.args = {
   muted: true,
   playsInline: true,
   className: "Native",
-  onReady: e => debug("onReady callback", e),
-  onPlay: e => debug("onPlay callback", e),
-  onPause: e => debug("onPause callback", e),
-  onEnded: e => debug("onEnded callback", e)
+  onReady: (e) => debug("onReady callback", e),
+  onPlay: (e) => debug("onPlay callback", e),
+  onPause: (e) => debug("onPause callback", e),
+  onEnded: (e) => debug("onEnded callback", e),
 };
