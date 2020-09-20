ae87024b42f61c3a707708fff6c4212270b70531
willybrauner <mail@willybrauner.com>
2020-09-20 21:08:44 +0200

    Init first test on document.title

--- packages/utils/metas-manager/test/MetasManager.test.ts
+++ packages/utils/metas-manager/test/MetasManager.test.ts
@@ -1,7 +1,21 @@
-import { MetasManager } from "../src/MetasManager";
+import MetasManager from "../src";
 
 describe("MetasManager", () => {
   it("should be defined", () => {
     expect(MetasManager).toBeDefined();
+  });
+
+  beforeEach(() => {});
+
+  it("should set default metas as reference", () => {});
+
+  // injecter des metas, verifier qu'ils sont bien setés dans le head
+  it("should inject metas values in head", () => {
+    console.log("document.title", document.title);
+
+    MetasManager.inject({ title: "Hello MetasManager" });
+
+    console.log("document.title", document.title);
+    expect(document.title).toBe("Hello MetasManager");
   });
 });
