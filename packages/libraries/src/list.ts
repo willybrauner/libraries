fdc9b24e82abd082e38a63512215e12f5d6e3d08
Willy Brauner <mail@willybrauner.com>
2021-06-27 22:21:46 +0200

    test libraries export

---
+++ packages/libraries/src/list.ts
@@ -0,0 +1,28 @@
+/**
+ * libraries standalone
+ * @description: export every modules from libraries repository.
+ */
+
+/**
+ * react-components
+ */
+export { GridLayout, EOrientation } from "@wbe/react-grid-layout";
+export { VideoPlayer, NativeVideo, VimeoVideo, YoutubeVideo, EVideoType } from "@wbe/react-video-player";
+export { Metas, TMetasProps } from "@wbe/react-metas";
+
+/**
+ * react-hooks
+ */
+export { useAsyncEffect, useAsyncLayoutEffect } from "@wbe/use-async-effect";
+export { useBoundingClientRect, EListener } from "@wbe/use-bounding-client-rect";
+export { useDidUpdate } from "@wbe/use-did-update";
+export { useDocumentScrollTop } from "@wbe/use-document-scroll-top";
+export { useIsInViewport } from "@wbe/use-is-in-viewport";
+export { useWindowSize, IWindowSize } from "@wbe/use-window-size";
+
+/**
+ * utils
+ */
+export { FakeDataUtils, EFakeVideoType, IFakeImage } from "@wbe/fake-data-utils"
+export { MetasManager, TTag, TMetaTags } from "@wbe/metas-manager"
+export { preloadImage, preloadImages } from "@wbe/preloads"
